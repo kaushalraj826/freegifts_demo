abae0c370c293329dfb1df2739f7f84528afe426
kaushalraj826 <85862648+kaushalraj826@users.noreply.github.com>
2026-03-27 15:38:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+let users = {};
+
+// join
+app.post("/join", (req, res) => {
+  const { userId } = req.body;
+
+  if (!users[userId]) {
+    users[userId] = { coins: 100, lastDaily: 0 };
+  }
+
+  res.json(users[userId]);
+});
+
+// daily bonus
+app.post("/daily", (req, res) => {
+  const { userId } = req.body;
+  let now = Date.now();
+
+  if (now - users[userId].lastDaily > 86400000) {
+    users[userId].coins += 20;
+    users[userId].lastDaily = now;
+    return res.json({ success: true, coins: users[userId].coins });
+  }
+
+  res.json({ success: false, msg: "Already claimed" });
+});
+
+// spin
+app.post("/spin", (req, res) => {
+  const { userId } = req.body;
+
+  let reward = Math.floor(Math.random() * 50);
+  users[userId].coins += reward;
+
+  res.json({ reward, coins: users[userId].coins });
+});
+
+app.listen(3000, () => console.log("Server running"));
